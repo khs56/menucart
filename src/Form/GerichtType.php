da33785170c0c18e75d160558ebc07b392e5987f
khs56 <vishal.mudi@esd.ny.gov>
2022-05-24 14:07:16 -0400

    Update GerichtType.php

--- src/Form/GerichtType.php
+++ src/Form/GerichtType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Gericht;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,6 +15,7 @@ class GerichtType extends AbstractType
     {
         $builder
             ->add('name')
+            ->add('image_file', FileType::class, ['mapped' => false])
             ->add('description')
             ->add('price')
             ->add('save', SubmitType::class)
