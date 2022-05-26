78f0dcd7cb111b400c9511c03731e3ee215033a7
kittyhawk-sailor <48333506+VishalESD@users.noreply.github.com>
2022-05-26 15:34:08 -0400

    Update HomeController.php

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\GerichtRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(GerichtRepository $gr)
     {
+        $dish = $gr->findAll();
+        $random = array_rand($dish, 2);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            // 'controller_name' => 'HomeController',
+            'g1' => $dish[$random[0]],
+            'g2' => $dish[$random[1]],
         ]);
     }
     
